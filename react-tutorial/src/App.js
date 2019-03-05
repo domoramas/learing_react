6e1423dfc3b50f0ac46e58ed2c5414c4c0ad5548
domoramas <boombappro@gmail.com>
2019-03-05 15:38:08 -0800

    added form

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,27 +1,11 @@
 import React, {Component} from 'react';
 import Table from './Table';
+import Form from './Forms';
+
 
 class App extends Component {
   state = {
-    characters:[
-      {
-        'name': 'Charlie',
-        'job': 'Janitor'
-    },
-    {
-        'name': 'Mac',
-        'job': 'Bouncer'
-    },
-    {
-        'name': 'Dee',
-        'job': 'Aspring actress'
-    },
-    {
-        'name': 'Dennis',
-        'job': 'Bartender'
-    }
-
-    ]
+    characters:[]
   };
   removeCharacter = index => {
     const { characters } = this.state;
@@ -32,6 +16,9 @@ class App extends Component {
         })
     });
   }
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
+}
   render() {
   const {characters } = this.state;
 
@@ -40,6 +27,7 @@ class App extends Component {
             <Table 
             characterData={characters}
             removeCharacter ={this.removeCharacter} />
+            <Form handleSubmit={this.handleSubmit}/>
         </div>
     );
   }
