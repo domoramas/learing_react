16cd47b52c7944c3e2e30e8349751337c8cc48d0
domoramas <boombappro@gmail.com>
2019-03-06 11:43:23 -0800

    form completed for zip code

---
+++ react-weather/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Form from './Forms';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "container">
+        <Form />
+      </div>
+      
+    );
+  }
+}
+
+export default App;
---
+++ react-weather/src/Forms.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react"
+
+class Form extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange = event => {
+    this.setState({value: event.target.value})
+  }
+  handleSubmit = event => {
+    alert('this zip was submitted: ' + this.state.value );
+    event.preventDefault();
+  }
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label> Enter your zip code:
+        <input 
+          type="text"
+          name= "zip"
+          value={this.state.value}
+          onChange={this.handleChange} />
+          </label>
+          <input 
+          type="submit"
+          value = "Submit" />
+      </form>
+    );
+  }
+}
+
+export default Form
