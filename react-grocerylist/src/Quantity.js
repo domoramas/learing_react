26dd4b6a92075e62d0d063e1b0fd110f7d7f4580
domoramas <boombappro@gmail.com>
2019-04-19 11:31:21 -0700

    fixed code to stop negative quantity

--- react-grocerylist/src/Quantity.js
+++ react-grocerylist/src/Quantity.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 const Quantity = ({index, quantity, changeQuantity}) => {
     return (
       <div className="counter">
-        <button className="counter-action decrement" onClick={()=>changeQuantity(index, -1)} > - </button>
+        <button className="counter-action decrement" disabled={quantity<1} onClick={()=>changeQuantity(index, -1)} > - </button>
         <span className="counter-quantity">{ quantity }</span>
         <button className="counter-action increment" onClick={()=>changeQuantity(index, +1)}> + </button>
       </div>
